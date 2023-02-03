c1be460bba09d3fe7bc6e67c97b037c797eae00d
JayrajBitake <108736732+JayrajBitake@users.noreply.github.com>
2023-02-03 13:25:39 +0530

    Add files via upload

---
+++ Controllers/ComplainLogic.cs
@@ -0,0 +1,81 @@
+﻿using FinalProject.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace FinalProject.Controllers
+{
+    public class ComplainLogic
+    {
+        public IEnumerable<Complain> complain
+        {
+            get
+            {
+
+                List<Complain> c1 = new List<Complain>();
+                using (SqlConnection con = new SqlConnection(("Data Source=(localdb)\\ProjectModels;Initial Catalog=FinalProject;Integrated Security=True;")))
+                {
+                    SqlCommand cmd = new SqlCommand("GetComplainDetails", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        Complain c = new Complain();
+                        c.Id = Convert.ToInt32(rdr["Id"]);
+                        c.Category = rdr["Category"].ToString();
+                        c.Description = rdr["Description"].ToString();
+                        c.Status = rdr["Status"].ToString();
+                        c1.Add(c);
+                    }
+                }
+                return c1;
+            }
+        
+
+
+
+        }
+        public void UpdateStatus(Complain complain)
+        {
+            using (SqlConnection con = new SqlConnection(("Data Source=(localdb)\\ProjectModels;Initial Catalog=FinalProject;Integrated Security=True;")))
+            {
+                SqlCommand cmd = new SqlCommand("Update", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter Id = new SqlParameter();
+                Id.ParameterName = "@Id";
+                Id.Value = complain.Id;
+                cmd.Parameters.Add(Id);
+                SqlParameter Status = new SqlParameter();
+                Status.ParameterName = "@Status";
+                Status.Value = complain.Id;
+                cmd.Parameters.Add(Status);
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+
+
+            }
+
+            }
+        public void DeleteComplain(int id)
+        {
+            using (SqlConnection con = new SqlConnection(("Data Source=(localdb)\\ProjectModels;Initial Catalog=FinalProject;Integrated Security=True;")))
+
+         
+            {
+                SqlCommand cmd = new SqlCommand("Delete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter paramId = new SqlParameter();
+                paramId.ParameterName = "@Id";
+                paramId.Value = id;
+                cmd.Parameters.Add(paramId);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,159 @@
+﻿using FinalProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace FinalProject.Controllers
+{
+    public class UserController : Controller
+    {
+        db dbop = new db();
+        // GET: UserController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Enroll e)
+        {
+            Enroll er = new Enroll();
+            using (SqlConnection con = new SqlConnection("Data Source=(localdb)\\ProjectModels;Initial Catalog=FinalProject;Integrated Security=True;"))
+            {
+                using (SqlCommand cmd = new SqlCommand("EnrollDetail", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@LoginName", e.LoginName);
+                    cmd.Parameters.AddWithValue("@FullName", e.FullName);
+                    cmd.Parameters.AddWithValue("@Password", e.Password);
+                    cmd.Parameters.AddWithValue("@ConfirmPassword", e.ConfirmPassword);
+                    cmd.Parameters.AddWithValue("@Gender", e.Gender);
+                    cmd.Parameters.AddWithValue("@EmailId", e.EmailId);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", e.PhoneNumber);
+
+                    // cmd.Parameters.AddWithValue("@status", "INSERT");
+                    con.Open();
+                    ViewData["result"] = cmd.ExecuteNonQuery();
+                    con.Close();
+
+                }
+            }
+
+
+            return View();
+        }
+
+
+        // GET: UserController/Details/5
+        public ActionResult Details([Bind] ULogin ul)
+        {
+            try
+            {
+                db dbop = new db();
+                int res = dbop.LoginCheck(ul);
+                if (res == 1)
+                {
+                    TempData["msg"] = "Welcome User..!!";
+                    return View("Create");
+                }
+                else
+                {
+                    TempData["msg"] = "Login Name or Password is wrong.!";
+                }
+
+
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Complain c)
+        {
+            try
+            {
+                Enroll er = new Enroll();
+                using (SqlConnection con = new SqlConnection("Data Source=(localdb)\\ProjectModels;Initial Catalog=FinalProject;Integrated Security=True;"))
+                {
+                    using (SqlCommand cmd = new SqlCommand("EnrollComplain", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Id", c.Id);
+                        cmd.Parameters.AddWithValue("@Category", c.Category);
+                        cmd.Parameters.AddWithValue("@Description", c.Description);
+                        cmd.Parameters.AddWithValue("@Status", c.Status);
+
+
+                        // cmd.Parameters.AddWithValue("@status", "INSERT");
+                        con.Open();
+                        ViewData["result"] = cmd.ExecuteNonQuery();
+                        con.Close();
+
+                    }
+                }
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
